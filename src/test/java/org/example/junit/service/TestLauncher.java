0f4b60e87c6596a2d8926ccd8bbfdc1b293be94f
79855 <leb.alex123@gmail.com>
2023-01-30 14:44:32 +0300

    Launcher API. Запуск тестов

---
+++ src/test/java/org/example/junit/service/TestLauncher.java
@@ -0,0 +1,28 @@
+package org.example.junit.service;
+
+import org.junit.platform.engine.discovery.DiscoverySelectors;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
+
+import java.io.PrintWriter;
+
+public class TestLauncher {
+
+    public static void main(String[] args) {
+        var launcher = LauncherFactory.create();
+
+        var summaryGeneratingListener = new SummaryGeneratingListener();
+
+        var request = LauncherDiscoveryRequestBuilder
+                .request()
+                .selectors(DiscoverySelectors.selectPackage("org.example.junit.service"))
+                .build();
+
+        launcher.execute(request, summaryGeneratingListener);
+
+        try (var writer = new PrintWriter(System.out)) {
+            summaryGeneratingListener.getSummary().printTo(writer);
+        }
+    }
+}
